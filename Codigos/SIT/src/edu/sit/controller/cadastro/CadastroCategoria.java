8d9eb5a93959ff34c9417185e0e93a3bfe1af1dd
LEO .. <leo_26198@hotmail.com>
2018-11-12 18:57:15 -0200

    asdasdsadas

---
+++ Codigos/SIT/src/edu/sit/controller/cadastro/CadastroCategoria.java
@@ -0,0 +1,32 @@
+package edu.sit.controller.cadastro;
+
+import edu.sit.bancodedados.conexao.ConexaoException;
+import edu.sit.bancodedados.dao.ContatoDao;
+import edu.sit.erro.cadastro.CadastroExeption;
+import edu.sit.erro.cadastro.EErroCadastro;
+import edu.sit.erros.dao.DaoException;
+import edu.sit.model.Contato;
+import edu.sit.uteis.Leitor;
+import edu.sit.view.menu.LeituraException;
+
+public class CadastroCategoria {
+
+	public static boolean cadastraCategoria() throws CadastroExeption, ConexaoException {
+		String nome = null;
+		System.out.print("*****CADASTRO DE CATEGORIA*****");
+
+		while (nome == null) {
+			try {
+				System.out.print("Telefone:\t");
+				nome = Leitor.leString();
+			} catch (LeituraException e) {
+				System.out.println(e.getMessage());
+			}
+		}
+				try {
+		} catch (DaoException e) {
+			throw new CadastroExeption(EErroCadastro.ERRO_CADASTRO_CONTATO);
+		}
+		return false;
+	}
+}
