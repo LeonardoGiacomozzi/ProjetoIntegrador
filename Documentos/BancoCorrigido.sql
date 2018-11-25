59ef5a380f1787a3e6d06cc21a99d0de61a5fe8e
LEO .. <leo_26198@hotmail.com>
2018-11-25 01:22:18 -0200

    Banco Att2

--- Documentos/BancoCorrigido.sql
+++ Documentos/BancoCorrigido.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA IF NOT EXISTS `Tabacaria` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `Tabacaria`  DEFAULT CHARACTER SET utf8 ;
 USE `Tabacaria` ;
 
 -- -----------------------------------------------------
