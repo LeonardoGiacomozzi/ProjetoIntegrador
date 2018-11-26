4bf554fd332d56d2b5d082f7727f87013a8df60a
pH <hericles_peixer@hotmail.com>
2018-11-26 13:41:10 -0200

    ah

--- Codigos/SIT/src/edu/sit/view/menu/Cadastros.java
+++ Codigos/SIT/src/edu/sit/view/menu/Cadastros.java
@@ -1,9 +1,19 @@
 package edu.sit.view.menu;
 
+import edu.sit.uteis.Leitor;
+
 public class Cadastros {
-	
+
 	public static void montaMenuCadastroCliente() throws TabacariaException {
-	
+		try {
+			System.out.println("CNPJ: \t");
+			Leitor.leCnpj();
+		}
+		
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+		
 	}
 
 	public static void montaMenuFuncionario() throws TabacariaException {
