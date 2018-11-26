01165cf0142a17007152a7c5448592ee3eaa9350
pH <hericles_peixer@hotmail.com>
2018-11-26 19:20:07 -0200

    xd

--- Codigos/SIT/src/edu/sit/view/menu/Cadastros.java
+++ Codigos/SIT/src/edu/sit/view/menu/Cadastros.java
@@ -6,12 +6,16 @@ public class Cadastros {
 
 	public static void montaMenuCadastroCliente() throws TabacariaException {
 		try {
-			System.out.println("CNPJ: \t");
-			Leitor.leCnpj();
-		}
-		
-		catch (Exception e) {
-			// TODO: handle exception
+			System.out.print("Nome: \t\t\t");
+			Leitor.leString();
+			System.out.print("CPF: \t\t");
+			Leitor.leCpf();
+			System.out.print("Data de Nascimento: \t");
+			Leitor.leData();
+			System.out.print("Endereço: \t\t");
+			Leitor.leString();
+		} catch (Exception e) {
+			System.out.println(EMensagensErroCad.ERRO_ESCRITA);
 		}
 		
 	}
