50561d6656917807f2f6cb30c0e38e65eb7bc4cc
LEO .. <leo_26198@hotmail.com>
2018-11-27 10:36:36 -0200

    Metodo efetua login ok

--- Codigos/SIT/src/edu/sit/controller/cadastro/UsuarioController.java
+++ Codigos/SIT/src/edu/sit/controller/cadastro/UsuarioController.java
@@ -8,25 +8,44 @@ import edu.sit.model.Usuario;
 import edu.sit.uteis.cadastro.UtilCadastro;
 
 public class UsuarioController {
-	
-	
+
 	public static boolean cadastro() throws CadastroException {
-		
+
 		String login = null;
 		String senha = null;
 		System.out.println("*********CADASTRO DE USUARIO*********");
 		login = UtilCadastro.pedeNome();
 		senha = UtilCadastro.pedeSenha();
-		
+
 		try {
-			System.out.println(new UsuarioDao().insere( Usuario.criaUsuario(login, senha))?"Usuario cadastrado com SUCESSO!!": "FATAL_ERROR");
+			System.out.println(
+					new UsuarioDao().insere(Usuario.criaUsuario(login, senha)) ? "Usuario cadastrado com SUCESSO!!"
+							: "FATAL_ERROR");
 			return true;
 		} catch (DaoException | ConexaoException e) {
 			System.out.println(e.getMessage());
 			throw new CadastroException(EErroCadastro.ERRO_CADASTRO_USUARIO);
 		}
-		
-		
+
 	}
-	
+
+	public static boolean efetuaLogin(Usuario entrando) {
+
+		try {
+			Usuario doBanco = new UsuarioDao().consulta(entrando.getLogin());
+			if (doBanco.equals(entrando)) {
+				
+				return true;
+			}
+			System.out.println("Senha Invalida");
+			return false;
+		} catch (DaoException e) {
+			System.out.println(e.getMessage() + "\n Usuario não encontrado\n");
+			return false;
+		} catch (ConexaoException e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+	}
+
 }
