3d89e30ddada7a9b63d465318f6f7346208db88f
pH <hericles_peixer@hotmail.com>
2018-11-27 20:47:00 -0200

    dsahdsauidsay

--- Codigos/SIT/src/edu/sit/propriedades/Configuracao.java
+++ Codigos/SIT/src/edu/sit/propriedades/Configuracao.java
@@ -2,29 +2,26 @@ package edu.sit.propriedades;
 
 import java.util.ArrayList;
 
-import com.mysql.cj.x.protobuf.MysqlxCrud.Collection;
-
-import edu.sit.model.Produto;
 import edu.sit.uteis.Arquivo;
 import edu.sit.uteis.cadastro.UtilCadastro;
 
 public class Configuracao {
 	
-	
 	public boolean setPropriedades() {
-		
-		
-		String banco = "banco;"+UtilCadastro.pedeNome("Informe o nome do banco de dados");
-		String usuario = "usuario;"+UtilCadastro.pedeNome("Informe o nome do usuario do banco de dados");
-		String senha = "senha;"+UtilCadastro.pedeNome("Informe a senha de acesso");
-		String localNotas = "localNota;"+UtilCadastro.pedeNome("Informe o local onde deseja salvar as notas fiscais");
-		
+
+		String banco = "banco;" + UtilCadastro.pedeNome("Informe o nome do banco de dados");
+		String usuario = "usuario;" + UtilCadastro.pedeNome("Informe o nome do usuario do banco de dados");
+		String senha = "senha;" + UtilCadastro.pedeNome("Informe a senha de acesso");
+		String localNotas = "localNota;" + UtilCadastro.pedeNome("Informe o local onde deseja salvar as notas fiscais");
+
 		ArrayList<String> props = new ArrayList<>();
 		props.add(banco);
 		props.add(usuario);
 		props.add(senha);
 		props.add(localNotas);
-		
-		Arquivo.gravaArquivo("propiedades.txt",props, false);
+
+		Arquivo.gravaArquivo("propiedades.txt", props, false);
+		return true;
 	}
+
 }
