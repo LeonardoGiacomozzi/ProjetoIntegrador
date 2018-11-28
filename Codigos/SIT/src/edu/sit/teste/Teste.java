268b5c22cbcc30e7085c8c6b9b76b6fb05669179
edufsm <edu.0203@hotmail.com>
2018-11-28 19:24:40 -0200

    tem erros

---
+++ Codigos/SIT/src/edu/sit/teste/Teste.java
@@ -0,0 +1,13 @@
+package edu.sit.teste;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class Teste {
+
+	@Test
+	public void 
+	
+
+}
