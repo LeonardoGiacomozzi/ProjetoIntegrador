9ee580973fb0b683455ffca5db58f3ff8e3062e4
edufsm <edu.0203@hotmail.com>
2018-11-30 22:32:27 -0200

    Testes (apenas alguns)

--- Codigos/SIT/src/edu/sit/teste/Teste.java
+++ Codigos/SIT/src/edu/sit/teste/Teste.java
@@ -1,13 +1,50 @@
 package edu.sit.teste;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import org.junit.Test;
+
+import edu.sit.bancodedados.conexao.ConexaoException;
+import edu.sit.bancodedados.dao.VendaDao;
+import edu.sit.controller.cadastro.UsuarioController;
+import edu.sit.controller.notaFiscal.GeraArquivoNotaFiscal;
+import edu.sit.erro.controller.ControllerException;
+import edu.sit.erro.notaFiscal.NotaFiscalException;
+import edu.sit.erros.dao.DaoException;
+import edu.sit.model.NotaFiscal;
+import edu.sit.model.Usuario;
+import edu.sit.uteis.CalculoIdade;
 
 public class Teste {
 
 	@Test
-	public void 
+	public void testaIdadeValida() {
+
+		assertEquals(18,
+				CalculoIdade.calculaIdade(LocalDate.parse("02/03/2000", DateTimeFormatter.ofPattern("dd/MM/yyyy"))));
+	}
 	
+
+	@Test
+	public void testaValidaLogin() throws DaoException, ConexaoException {
+
+		assertEquals(true, UsuarioController.validaLogin(Usuario.criaUsuario("leo", "123")));
+
+	}
+
+	@Test
+	public void testaGeraNotaFiscal() throws DaoException, ConexaoException, NotaFiscalException {
+
+		assertEquals(true,
+				GeraArquivoNotaFiscal.geraArquivo(NotaFiscal.criaNotaFiscal(new VendaDao().consultaCompleta(1))));
+	}
+	
+	@Test
+	public void testaEfetuaVenda() throws ControllerException {
+		
+	}
 
 }
