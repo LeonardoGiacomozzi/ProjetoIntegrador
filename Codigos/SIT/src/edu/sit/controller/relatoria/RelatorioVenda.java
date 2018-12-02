ebcbb5bc9e16051d819919b95f177db8e50b239b
LEO .. <leo_26198@hotmail.com>
2018-12-02 10:28:52 -0200

    Retatório OK

--- Codigos/SIT/src/edu/sit/controller/relatoria/RelatorioVenda.java
+++ Codigos/SIT/src/edu/sit/controller/relatoria/RelatorioVenda.java
@@ -1,5 +1,37 @@
 package edu.sit.controller.relatoria;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import edu.sit.erro.notaFiscal.EErroNotaFiscal;
+import edu.sit.erro.notaFiscal.NotaFiscalException;
+import edu.sit.model.Venda;
+import edu.sit.uteis.Arquivo;
+import edu.sit.uteis.cadastro.UtilCadastro;
+
 public class RelatorioVenda {
 
+public static boolean geraArquivo(ArrayList<Venda> vendas) throws NotaFiscalException {
+		
+		List<String> palavras = new ArrayList<String>();
+		palavras.add("");
+		for (Venda venda : vendas) {
+			palavras.add("Funcionario "+venda.getFuncionario().getNome()
+					+"\tCliente "+venda.getCliente().getNome()
+					+"\tValor "+ venda.getValor()
+					+"\tData da venda "+venda.getDataVenda());
+		}
+		String path=UtilCadastro.pedeNome("Informe onde deseja salvar o relatório: ")+"\\Relatorio"+LocalDate.now().toString()+".txt";
+		try {
+			Arquivo.gravaArquivo(path, palavras, false);
+		} catch (IOException e) {
+			
+			System.out.println(e.getMessage());
+			throw new NotaFiscalException(EErroNotaFiscal.ERRO_GRAVA_ARQUIVO);
+		}
+		return true;
+	}
 }
