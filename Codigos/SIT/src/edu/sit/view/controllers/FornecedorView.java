aadb65632063c56816a308e7f6bba23eeb962f72
LEO .. <leo_26198@hotmail.com>
2018-12-08 19:55:03 -0200

    Adicioonando exclui Cat/Fun/Forn

--- Codigos/SIT/src/edu/sit/view/controllers/FornecedorView.java
+++ Codigos/SIT/src/edu/sit/view/controllers/FornecedorView.java
@@ -2,24 +2,24 @@ package edu.sit.view.controllers;
 
 import edu.sit.bancodedados.conexao.ConexaoException;
 import edu.sit.bancodedados.dao.FornecedorDao;
+import edu.sit.controller.cadastro.FornecedorController;
+import edu.sit.erro.leitura.LeituraException;
 import edu.sit.erro.visualizacao.EErroVisualizacao;
 import edu.sit.erro.visualizacao.VisualizacaoException;
 import edu.sit.erros.dao.DaoException;
 import edu.sit.model.Fornecedor;
+import edu.sit.uteis.Leitor;
 
 public class FornecedorView {
-	
+
 	public static boolean visualizar() throws VisualizacaoException {
 
 		try {
 			System.out.println("\n**** LISTA DE FUNCIONÁRIOS ****\n");
-			System.out.println(String.format("%-15s", "Código") + 
-							   String.format("%-20s", "Nome") + 
-							   "CNPJ");
+			System.out.println(String.format("%-15s", "Código") + String.format("%-20s", "Nome") + "CNPJ");
 			for (Fornecedor fornecedor : new FornecedorDao().consultaTodos()) {
-				System.out.println(String.format("%-15s", "[" + fornecedor.getId() + "]") + 
-								   String.format("%-20s", fornecedor.getNome()) + 
-								   String.format("%-10s", fornecedor.getCNPJ()));
+				System.out.println(String.format("%-15s", "[" + fornecedor.getId() + "]")
+						+ String.format("%-20s", fornecedor.getNome()) + String.format("%-10s", fornecedor.getCNPJ()));
 			}
 			return true;
 		} catch (DaoException | ConexaoException e) {
@@ -27,13 +27,37 @@ public class FornecedorView {
 			throw new VisualizacaoException(EErroVisualizacao.ERRO_BUSCA_FORNECEDORES);
 		}
 	}
-	
+
 	public static void exibeFornecedor(Fornecedor fornecedor) {
 		System.out.println("************* DADOS DO CLIENTE***************");
-		System.out.println("Nome\t"+fornecedor.getNome());
-		System.out.println("CNPJ\t"+fornecedor.getCNPJ());
-		System.out.println("Email\t"+fornecedor.getContato().getEmail()+"--------- Telefone\t"+fornecedor.getContato().getTelefone());
-		System.out.println("Pessoa responsável\t"+fornecedor.getPessoaResponsavel());
-	
+		System.out.println("Nome\t" + fornecedor.getNome());
+		System.out.println("CNPJ\t" + fornecedor.getCNPJ());
+		System.out.println("Email\t" + fornecedor.getContato().getEmail() + "--------- Telefone\t"
+				+ fornecedor.getContato().getTelefone());
+		System.out.println("Pessoa responsável\t" + fornecedor.getPessoaResponsavel());
+
 	}
+
+	public static boolean exclui() {
+
+		try {
+			visualizar();
+			Integer op = null;
+			System.out.println("ESCOLHA QUAL FORNECEDOR DESEJA EXCLUIR ");
+			do {
+			try {
+				op = Leitor.leInteger();
+			} catch (LeituraException e) {
+				System.out.println(e.getMessage());
+			}
+			}while(op==null);
+			 return FornecedorController.exclui(op);
+			
+		} catch (VisualizacaoException e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+
+	}
+
 }
--- Codigos/SIT/src/edu/sit/view/controllers/FuncionarioView.java
+++ Codigos/SIT/src/edu/sit/view/controllers/FuncionarioView.java
@@ -2,10 +2,13 @@ package edu.sit.view.controllers;
 
 import edu.sit.bancodedados.conexao.ConexaoException;
 import edu.sit.bancodedados.dao.FuncionarioDao;
+import edu.sit.controller.cadastro.FuncionarioController;
+import edu.sit.erro.leitura.LeituraException;
 import edu.sit.erro.visualizacao.EErroVisualizacao;
 import edu.sit.erro.visualizacao.VisualizacaoException;
 import edu.sit.erros.dao.DaoException;
 import edu.sit.model.Funcionario;
+import edu.sit.uteis.Leitor;
 
 public class FuncionarioView {
 
@@ -13,13 +16,10 @@ public class FuncionarioView {
 
 		try {
 			System.out.println("\n**** LISTA DE FUNCIONÁRIOS ****\n");
-			System.out.println(String.format("%-10s", "Código") + 
-							   String.format("%-30s", "Nome") + 
-							   "CPF");
+			System.out.println(String.format("%-10s", "Código") + String.format("%-30s", "Nome") + "CPF");
 			for (Funcionario funcionario : new FuncionarioDao().consultaTodos()) {
-				System.out.println(String.format("%-10s", "[" + funcionario.getId() + "]") +  
-								   String.format("%-30s", funcionario.getNome()) +
-								   String.format("%-10s", funcionario.getCpf()));
+				System.out.println(String.format("%-10s", "[" + funcionario.getId() + "]")
+						+ String.format("%-30s", funcionario.getNome()) + String.format("%-10s", funcionario.getCpf()));
 			}
 			return true;
 		} catch (DaoException | ConexaoException e) {
@@ -27,5 +27,26 @@ public class FuncionarioView {
 			throw new VisualizacaoException(EErroVisualizacao.ERRO_BUSCA_FUNCIONARIOS);
 		}
 	}
-	
+
+	public static boolean excluir() {
+		try {
+			visualizar();
+			Integer op = null;
+
+			do {
+
+				try {
+
+					System.out.println("ESCOLHA O FUNCIONARIO QUE QUER EXCLUIR");
+					op = Leitor.leInteger();
+				} catch (LeituraException e) {
+					System.out.println(e.getMessage());
+				}
+			} while (op == null);
+			return FuncionarioController.exclui(op);
+		} catch (VisualizacaoException e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+	}
 }
